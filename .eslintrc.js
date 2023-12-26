8ef45cc47a0c60dd9db5f3a4df71fae44ab68d2d
sniper1794 <sniper1794@mail.ru>
2023-12-27 02:57:20 +0300

    eslit.js;

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:i18next/recommended',
+
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+        'i18next',
+    ],
+
+    rules: {
+    /* 2- run rules; 4 -  отступы в пробелах */
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        indent: [2, 4],
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off', // отключает предпочтение дефолтному экспорту
+        'no-unused-vars': 'warn', // Предупреждение на неизпольз. переменные
+        'react/require-default-props': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'warn', // Предупрежение что используем spread(...) с pops
+        'react/function-component-definition': 'off', // отключение предпочтения function  стрелочкной функции
+        'no-shadow': 'off',
+        'import/extensions': 'off', // отключение ошибки что нет  расширений(.ts) в импортах
+        'import/no-extraneous-dependencies': 'off',
+        'no-underscore-dangle': 'off',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
+    },
+    globals: {
+        __IS_DEV__: true,
+    },
+};
